b5939567c6e0ba8b251b5600f3a354ae16bc0793
az1628 <44253932+az1628@users.noreply.github.com>
2020-03-16 06:15:09 -0400

    b

--- src/transformers/data/processors/glue.py
+++ src/transformers/data/processors/glue.py
@@ -564,8 +564,8 @@ class BoolqProcessor(DataProcessor):
             if i == 0:
                 continue
             guid = "%s-%s" % (set_type, i)
-            text_a = line['question']
-            text_b = line['answer']
+            text_a = line['passage']
+            text_b = line['question']
             label = line['label']
             examples.append(InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
         return examples
